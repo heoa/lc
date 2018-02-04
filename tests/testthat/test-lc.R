3e9490212dd0eceec557c2411d0debb49bec1577
Thomas Mailund <thomas.mailund@gmail.com>
2018-02-04 11:45:19 +0100

    Got to 100% coverage.

--- tests/testthat/test-lc.R
+++ tests/testthat/test-lc.R
@@ -5,3 +5,10 @@ test_that("We can do a simple map using lc", {
   y <- unlist(lc(x ** 2, x = x))
   expect_equal(y, x ** 2)
 })
+
+test_that("We can filter elements using predicates", {
+  x <- 1:10
+  y <- x[x %% 2 == 0]
+  z <- unlist(lc(x, x = x, x %% 2 == 0))
+  expect_equal(y, z)
+})
